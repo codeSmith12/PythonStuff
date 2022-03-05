3e90cb3ced4bf98ae004b8ba858b372810603d3b
codeSmith12 <89124403+codeSmith12@users.noreply.github.com>
2022-03-04 17:30:07 -0800

    Added Grid to snake, got head of snake in the map but it's offset...

--- CWC/Friday/snake.py
+++ CWC/Friday/snake.py
@@ -1,19 +1,35 @@
 # snake.py
-import tkinter
+import tkinter, time
+
+class Snake:
+    def __init__(self):
+        self.x = SPEED
+        self.y = 0                  # x1,y1, x2,y2
+        self.head = canvas.create_rectangle(WIDTH/2, HEIGHT/2, WIDTH/2 + HEADSIZE, HEIGHT/2 + HEADSIZE, fill="white", tags=('snake'))
+    def updatePosition(self):
+        canvas.move(self.head, self.x, self.y)
 
 # Functions
 def drawGrid(): # 20
-    CELL_WIDTH = WIDTH // HEAD_SIZE
-    CELL_HEIGHT = HEIGHT // HEAD_SIZE
+    CELL_WIDTH = WIDTH // HEADSIZE
+    CELL_HEIGHT = HEIGHT // HEADSIZE
+    # Create horizontal lines
     for i in range(CELL_HEIGHT):
         canvas.create_line(0, CELL_HEIGHT*i, WIDTH, CELL_HEIGHT*i, fill="ivory3")
-    # TODO: Make loop for columns
+    # Create vertical lines
+    for i in range(CELL_WIDTH):
+        canvas.create_line(CELL_WIDTH*i, 0, CELL_WIDTH*i, HEIGHT, fill="ivory3")
+
+
 tk = tkinter.Tk()
 tk.title("Snake")
 # Constants
 WIDTH = 800
 HEIGHT = 800
-HEAD_SIZE = 25
+HEADSIZE = 25
+SPEED = HEADSIZE
+TICK = 0.15
+
 # Set the size of the window
 tk.geometry(f"{WIDTH}x{HEIGHT}")
 
@@ -21,9 +37,12 @@ canvas = tkinter.Canvas(tk, width=WIDTH, height=HEIGHT)
 # Change the background color to black
 canvas.configure(bg="black")
 canvas.pack()
+
 drawGrid()
-
-
+snake = Snake()
 
 # Keeps window open
-tk.mainloop()
+while True:
+    snake.updatePosition() # Moves snake
+    tk.update() # Keeps window open
+    time.sleep(TICK)
