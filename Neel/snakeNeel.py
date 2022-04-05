8f231f93c869f45c9da255b6ff288c930ba2a147
codeSmith12 <89124403+codeSmith12@users.noreply.github.com>
2022-04-04 19:26:37 -0700

    Got the bodyparts moving, hitboxes come along too!

--- Neel/snakeNeel.py
+++ Neel/snakeNeel.py
@@ -10,7 +10,7 @@ class Rat:
         self.y = self.y - (self.y % HEADSIZE)
         # Create hit box that is at the center of the rats cell
         self.hitBox = canvas.create_rectangle(self.x + MOUSESIZE/2, self.y+MOUSESIZE/2, self.x + MOUSESIZE/2, self.y + MOUSESIZE/2, fill = "black")
-        self.rat = canvas.create_rectangle(self.x, self.y, self.x + MOUSESIZE, self.y + MOUSESIZE, fill = "grey")
+        self.rat = canvas.create_rectangle(self.x, self.y, self.x + MOUSESIZE, self.y + MOUSESIZE, fill = MOUSECOLOR)
     def respawn(self):
         self.x = randint(0, WIDTH - MOUSESIZE)
         self.y = randint(0, HEIGHT - MOUSESIZE)
@@ -37,7 +37,7 @@ class Snake:
         self.bodyParts = []
         self.hitBoxes = []
         self.prevPos = []
-        self.head = canvas.create_rectangle(WIDTH/2, HEIGHT/2, WIDTH/2 + HEADSIZE, HEIGHT/2 + HEADSIZE, fill = "red")
+        self.head = canvas.create_rectangle(WIDTH/2, HEIGHT/2, WIDTH/2 + HEADSIZE, HEIGHT/2 + HEADSIZE, fill = SNAKECOLOR)
         tk.bind("<KeyPress-Up>", self.moveUp)
         tk.bind("<KeyPress-Down>", self.moveDown)
         tk.bind("<KeyPress-Right>", self.moveRight)
@@ -47,12 +47,14 @@ class Snake:
         global gameOver
         pos = canvas.coords(self.head)
         self.prevPos.insert(0,pos)
-        print(self.prevPos)
         if len(self.prevPos) > self.bodyCount+1:
             del self.prevPos[-1]
+        # Move the head to the next cell...
         canvas.move(self.head, self.x, self.y)
-
-
+        # Move each body part from the beginning to the end.
+        for i in range(len(self.bodyParts)):
+            canvas.moveto(self.hitBoxes[i], self.prevPos[i][0]+HEADSIZE//2, self.prevPos[i][1]+HEADSIZE//2)
+            canvas.moveto(self.bodyParts[i], self.prevPos[i][0], self.prevPos[i][1])
 
         self.directionLocked = False
         if pos[2] > WIDTH:
@@ -66,8 +68,10 @@ class Snake:
 
 
     def eatMouse(self):
+        hitBox = canvas.create_oval(self.prevPos[-1][0] + HEADSIZE//2, self.prevPos[-1][1] + HEADSIZE//2, self.prevPos[-1][0] + HEADSIZE//2, self.prevPos[-1][1] + HEADSIZE//2, fill="ivory3")
         bodyPart = canvas.create_oval(self.prevPos[-1][0], self.prevPos[-1][1], self.prevPos[-1][0] + HEADSIZE, self.prevPos[-1][1] + HEADSIZE, fill="ivory3")
         self.bodyParts.append(bodyPart)
+        self.hitBoxes.append(hitBox)
         self.bodyCount += 1
 
     def moveUp(self,evnt):
@@ -91,16 +95,33 @@ class Snake:
             self.x = -SPEED
             self.directionLocked = True
 
+    def checkIfTouchingBody(self):
+        pass
+def drawGrid():
+    cellWidth = WIDTH // COLS
+    cellHeight = HEIGHT // ROWS
+    for i in range(ROWS):
+        canvas.create_line(0, i*cellHeight, WIDTH, i*cellHeight, fill="ivory3")
+    for i in range(COLS):
+        canvas.create_line(i*cellWidth, 0, i*cellWidth, HEIGHT, fill="ivory3")
+
 
 tk = Tk()
 WIDTH=800
 HEIGHT=800
 HEADSIZE = 20
 MOUSESIZE = HEADSIZE
+SNAKECOLOR = "ivory3"
+GRIDCOLOR = SNAKECOLOR
+BACKGROUNDCOLOR = "black"
+MOUSECOLOR = "grey"
 
 
 assert WIDTH % HEADSIZE == 0,  "WIGHT does not divide evenly by Headsize"
 assert HEIGHT % HEADSIZE == 0, "HEIGHT does not divide evenly by Headsize"
+
+ROWS = HEIGHT//HEADSIZE
+COLS = WIDTH//HEADSIZE
 
 SPEED = HEADSIZE
 TICK = 0.1
@@ -110,9 +131,9 @@ tk.configure(bg="lime")
 
 tk.geometry(f"{WIDTH}x{HEIGHT}")
 
-canvas = Canvas(tk, width=WIDTH, height=HEIGHT, bg="lime")
+canvas = Canvas(tk, width=WIDTH, height=HEIGHT, bg=BACKGROUNDCOLOR)
 canvas.pack()
-
+# drawGrid()
 global gameOver
 
 gameOver = False
