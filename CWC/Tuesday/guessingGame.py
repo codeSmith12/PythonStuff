5354b824b79b770803a3f40a6ef58417d8c59838
codeSmith12 <89124403+codeSmith12@users.noreply.github.com>
2022-05-13 17:29:34 -0700

    Almost finished GuessingGame, Brandon needs help

--- CWC/Tuesday/guessingGame.py
+++ CWC/Tuesday/guessingGame.py
@@ -4,14 +4,35 @@ import turtle
 from random import randint, choice
 
 secret = randint(25,100)
-print(secret)
 pen = turtle.Turtle()
+# Maybe ask the user what background color they want, then use it.
+turtle.bgcolor("black")
+pen.color("spring green")
 shapes = ["square", "arrow", "circle", "turtle", "triangle", "classic"]
-pen.shape( choice(shapes) )
+pen.shape(choice(shapes))
 pen.forward(1)
+pen.up()
+pen.speed(0)
+
+# Draws "secret" amount of objects on the screen
+for i in range(secret):
+    pen.goto( randint(-400,400) , randint(-400,400) )
+    pen.stamp()
 
 
+tries = 0
+guess = 0 # Created variable so we can use in while condition
+while guess != secret:
+    # Input gives us back a string, always...
+    guess = input("How many objects are there? ")
+    guess = int(guess) # convert the guess to a number
+    tries += 1
+    if guess > secret:
+        print("Guess lower")
+    if guess < secret:
+        print("Guess higher")
 
+print(f"You got it in {tries} tries!!")
 
 
 
