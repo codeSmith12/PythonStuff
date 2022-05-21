9bf4a47cb82f26993bad89ea97a2978c8b2c4acc
codeSmith12 <89124403+codeSmith12@users.noreply.github.com>
2022-05-20 17:28:21 -0700

    Completed the place marker function!

--- CWC/Friday/connect4.py
+++ CWC/Friday/connect4.py
@@ -13,13 +13,6 @@ def drawGrid():
         for i in range(8): # i = 0,1,2,3,4,5,6,7
             canvas.create_oval(PADX + colWidth*i, PADY+colWidth*j, PADX + CIRCLE_SIZE + colWidth*i, PADY+CIRCLE_SIZE + colWidth*j, fill="white")
 
-def placeMarker(event):
-    # print(event.x,event.y)
-    colWidth = WIDTH/8 # == 100
-    rowClicked = event.x // colWidth # 99 // 100
-    print(rowClicked)
-
-
 def displayBoard(board):
     for row in board:
         for col in row:
@@ -27,13 +20,39 @@ def displayBoard(board):
         print("\n")
 
 
+def placeMarker(event):
+    global player1Turn
+    colWidth = WIDTH/8
+    colClicked = int(event.x // colWidth)
+    # Search bottom up for an empty slot.
+    for i in range(7, -1, -1):
+        # If the slot we are currently on is empty?
+        if board[i][colClicked] == 0:
+            # Place a marker for whoever's turn it is
+            if player1Turn:
+                board[i][colClicked] = 1
+                canvas.create_oval(PADX + colWidth*colClicked, PADY+colWidth*i, PADX + CIRCLE_SIZE + colWidth*colClicked, PADY+CIRCLE_SIZE + colWidth*i, fill="blue")
+            else:
+                board[i][colClicked] = 2
+                canvas.create_oval(PADX + colWidth*colClicked, PADY+colWidth*i, PADX + CIRCLE_SIZE + colWidth*colClicked, PADY+CIRCLE_SIZE + colWidth*i, fill="red")
+            # Flip the turn boolean variable
+            player1Turn = not player1Turn
+            break
+    displayBoard(board)
+# Created the canvas, write this below it \/
+
 # Game Constants
 WIDTH = 800
 HEIGHT = 800
-PADX = WIDTH//65 # <---
-PADY = HEIGHT//65 # <---
-CIRCLE_SIZE = WIDTH//10 # <---
+PADX = WIDTH//65
+PADY = HEIGHT//65
+CIRCLE_SIZE = WIDTH//10
 
+global player1Turn
+player1Turn = True
+
+# 1 in the board means player one's marker is there
+# 2 in the board means player two's marker is there
 
 global board
 board = []
