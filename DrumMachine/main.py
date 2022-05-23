9adf87a5a3e37d9b2c3adfdbb6d8ca7beffdeb0b
codeSmith12 <89124403+codeSmith12@users.noreply.github.com>
2022-05-23 16:25:42 -0700

    Up to 34:34 on video, added grid and just about to work on buttons

--- DrumMachine/main.py
+++ DrumMachine/main.py
@@ -2,19 +2,34 @@ import pygame
 from pygame import mixer
 
 
-def drawGrid():
+def drawGrid(clicked):
     leftBox = pygame.draw.rect(screen, gray, [0, 0, LEFTBOXWIDTH, HEIGHT-BOTTOMSIZE], BORDERWIDTH)
     bottomBox = pygame.draw.rect(screen, gray, [0, HEIGHT-BOTTOMSIZE, WIDTH, BOTTOMSIZE], BORDERWIDTH)
     boxes = []
     colors = [gray, white, gray]
 
     hiHatText = labelFont.render("Hi Hat", True, white)
-    screen.blit(hiHatText, (LEFTBOXWIDTH/3,HEIGHT/12))
+    screen.blit(hiHatText, (LEFTBOXWIDTH/3,50))
     snareText = labelFont.render("Snare", True, white)
-    screen.blit(snareText, (LEFTBOXWIDTH/3,HEIGHT*2/12))
+    screen.blit(snareText, (LEFTBOXWIDTH/3,150))
     kickText = labelFont.render("Kick", True, white)
-    screen.blit(kickText, (LEFTBOXWIDTH/3,HEIGHT*3/12))
+    screen.blit(kickText, (LEFTBOXWIDTH/3,250))
+    crashText = labelFont.render("Crash", True, white)
+    screen.blit(crashText, (LEFTBOXWIDTH/3,350))
+    clapText = labelFont.render("Clap", True, white)
+    screen.blit(clapText, (LEFTBOXWIDTH/3,440))
+    floorText = labelFont.render("Floor Tom", True, white)
+    screen.blit(floorText, (LEFTBOXWIDTH/4,530))
 
+    for i in range(1,instruments):
+        pygame.draw.line(screen, gray, [0, i*100], (LEFTBOXWIDTH, i*100), 3)
+
+    for i in range(beats):
+        for j in range(instruments):
+            rect = pygame.draw.rect(screen, gray, [i*((WIDTH-LEFTBOXWIDTH) // beats) + LEFTBOXWIDTH, (j*100), ((WIDTH-LEFTBOXWIDTH) // beats), ((HEIGHT-BOTTOMSIZE)//instruments)], 5, 5)
+            boxes.append((rect, (i,j)))
+
+    return boxes
 
 
 pygame.init()
@@ -36,16 +51,28 @@ labelFont = pygame.font.Font(None, 32)
 
 FPS = 60
 timer = pygame.time.Clock()
+beats = 8
+instruments = 6
+boxes = []
+clicked = [[-1 for _ in range(beats)] for _ in range(instruments)]
 
 run = True
 
 while run:
     timer.tick(FPS)
     screen.fill(black)
-    drawGrid()
+    boxes = drawGrid(clicked)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for i in range(len(boxes)):
+                if boxes[i][0].colliderect(event.pos):
+                    coords = boxes[i][1] # Grabs the tuple of x,y belonging to box
+                    clicked[coords[1]][coords[0]] *= -1
+
 
     pygame.display.flip()
+
 pygame.quit()
