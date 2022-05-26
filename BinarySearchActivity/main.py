0e4585d79b4eeb320d524337d8892f0ecdce1170
codeSmith12 <89124403+codeSmith12@users.noreply.github.com>
2022-05-25 19:56:44 -0700

    Base game setup

--- BinarySearchActivity/main.py
+++ BinarySearchActivity/main.py
@@ -7,8 +7,41 @@ class GuessingGame:
         self.min = min
         self.max = max
         self.secret = randint(min, max)
+        self.run = True
+        self.tries = 0
+
+        self.pen.speed(0)
+        self.pen.shape("turtle")
+        self.pen.color("cyan")
+        self.pen.up()
+        self.pen.hideturtle()
+        turtle.bgcolor("black")
+
 
     def displayObjects(self):
+        for i in range(self.secret):
+            self.pen.goto(randint(-400,400), randint(-400,400))
+            self.pen.stamp()
+
+
+    def playGame(self):
+        self.pen.clear()
+        self.displayObjects()
+
+        while self.run:
+            guess = int(input("How many turtles are there? "))
+            self.tries += 1
+
+            if guess < self.secret:
+                print("Guess higher")
+            elif guess > self.secret:
+                print("Guess lower")
+            else:
+                print(f"You got it in {self.tries} tries.")
+                self.run = False
+
+
+
 
 
 
@@ -16,4 +49,5 @@ class GuessingGame:
 
 
 game = GuessingGame(200,400)
+game.playGame()
 turtle.done()
